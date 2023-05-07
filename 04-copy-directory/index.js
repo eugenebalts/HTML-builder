01142c4f4c38a6effd45ca6dff7fb4298dc259bf
eugenebalts <norcobeatz@gmail.com>
2023-05-07 21:43:27 +0200

    feat: realize copy-dir function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,32 @@
-// const path = require('path');
-// const fs = require('fs');
-// const fsPromises = require('fs/promises');
-// const {copyFile, constants} = require('node:fs/promises');
+const path = require('path');
+const fs = require('fs');
+const {copyFile, readdir, mkdir, rm} = require('node:fs/promises');
 
-// function copyFiles(way) {
-//     fs.mkdir(path.join(__dirname, ), (err) => {
-//         if (err) {
-//             throw err.message;
-//             return;
-//         }
-//         console.log('Folder has crated')
-//     })
-// }
-//  copyFiles(path.join(__dirname, 'files'))
+async function copyFiles(folder) {
+
+    const copiedFolder = `${path.join(__dirname, folder)}`;
+    const pastedFolder = path.join(__dirname, `${folder}-copy`);
+    const copiedFiles = await readdir(copiedFolder, (err, files) => {
+        if (err) throw err;
+    });
+    // console.log(path.dirname(copiedFolder))
+    await mkdir(path.join(__dirname, `${folder}-copy`), {recursive: true});
+    await fs.readdir(pastedFolder, (err, files) => {
+        if (err) throw err;
+        for (const file of files) {
+            rm(path.join(pastedFolder, file))
+        }
+    })
+
+    for (const file of copiedFiles) {
+        // console.log(file)
+        await copyFile(path.join(copiedFolder, file), path.join(pastedFolder, file));
+        // console.log(await fsPromises.readFile(path.join(copiedFolder, file), 'utf-8'))
+    }
+}
+
+copyFiles('files')
+
 
 
 
